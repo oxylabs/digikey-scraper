128a3eb366c25a0fa1e473842d2b66c0e821d2fb
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:17:02 +0200

    Adding code examples/digikey_curl.sh

---
+++ code examples/digikey_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal_ecommerce", "url": "https://www.digikey.de/en/products/filter/industrial-automation-accessories/800"}'
